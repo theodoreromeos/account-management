fc3b58cc742b804bf8b213fe3bc0ebfe805cce06
theodoreromeos <theoromeos@gmail.com>
2026-03-20 13:46:18 +0200

    minor fixes

--- src/main/java/com/theodore/account/management/config/other/RabbitMqAutoConfiguration.java
+++ src/main/java/com/theodore/account/management/config/other/RabbitMqAutoConfiguration.java
@@ -3,20 +3,11 @@ package com.theodore.account.management.config.other;
 import com.theodore.queue.common.authserver.RollbackQueueConfig;
 import com.theodore.queue.common.config.CommonRabbitMqConfigs;
 import com.theodore.queue.common.emails.EmailQueueConfig;
-import com.theodore.queue.common.services.MessagingService;
-import com.theodore.queue.common.services.MessagingServiceImpl;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({CommonRabbitMqConfigs.class, EmailQueueConfig.class, RollbackQueueConfig.class})
 public class RabbitMqAutoConfiguration {
-
-    @Bean
-    MessagingService messagingService(RabbitTemplate rabbitTemplate) {
-        return new MessagingServiceImpl(rabbitTemplate);
-    }
 
 }
